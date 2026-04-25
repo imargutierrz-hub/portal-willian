bcd35ad2b52a0d410cc89668d683ff8bb7f5b7e3
Andres Gutierrez <imargutierrz@gmail.com>
2026-04-24 21:27:55 -0500

    fix postcss

--- escribir.mjs
+++ escribir.mjs
@@ -1,38 +1,11 @@
 import { writeFileSync } from 'fs'
 
-const postcss = `module.exports = {
+const postcss = `export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }`
 
-const tailwindconfig = `/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}`
-
-const globals = `@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-* {
-  box-sizing: border-box;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}`
-
 writeFileSync('postcss.config.mjs', postcss, 'utf8')
-writeFileSync('tailwind.config.js', tailwindconfig, 'utf8')
-writeFileSync('app/globals.css', globals, 'utf8')
 console.log('OK')
